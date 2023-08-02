6323ab670642581645d595560e713c9b2ec0c3fb
cky <cky3702@163.com>
2023-08-02 15:02:14 +0800

    add

--- configs/datasets/collections/base_small.py
+++ configs/datasets/collections/base_small.py
@@ -10,20 +10,20 @@ with read_base():
     # from ..FewCLUE_chid.FewCLUE_chid_ppl_8f2872 import chid_datasets
     # from ..FewCLUE_cluewsc.FewCLUE_cluewsc_ppl_868415 import cluewsc_datasets
     # from ..FewCLUE_eprstmt.FewCLUE_eprstmt_ppl_f1e631 import eprstmt_datasets
-    # from ..humaneval.humaneval_gen_8e312c import humaneval_datasets
-    # from ..mbpp.mbpp_gen_1e1056 import mbpp_datasets
+    from ..humaneval.humaneval_gen_8e312c import humaneval_datasets
+    from ..mbpp.mbpp_gen_1e1056 import mbpp_datasets
     from ..lambada.lambada_gen_217e11 import lambada_datasets
     from ..storycloze.storycloze_ppl_496661 import storycloze_datasets
-    # from ..SuperGLUE_AX_b.SuperGLUE_AX_b_ppl_6db806 import AX_b_datasets
-    # from ..SuperGLUE_AX_g.SuperGLUE_AX_g_ppl_66caf3 import AX_g_datasets
-    # from ..SuperGLUE_BoolQ.SuperGLUE_BoolQ_ppl_314b96 import BoolQ_datasets
-    # from ..SuperGLUE_CB.SuperGLUE_CB_ppl_0143fe import CB_datasets
-    # from ..SuperGLUE_COPA.SuperGLUE_COPA_ppl_9f3618 import COPA_datasets
-    # from ..SuperGLUE_MultiRC.SuperGLUE_MultiRC_ppl_ced824 import MultiRC_datasets
-    # from ..SuperGLUE_RTE.SuperGLUE_RTE_ppl_66caf3 import RTE_datasets
-    # from ..SuperGLUE_ReCoRD.SuperGLUE_ReCoRD_gen_30dea0 import ReCoRD_datasets
-    # from ..SuperGLUE_WiC.SuperGLUE_WiC_ppl_312de9 import WiC_datasets
-    # from ..SuperGLUE_WSC.SuperGLUE_WSC_ppl_d0f531 import WSC_datasets
+    from ..SuperGLUE_AX_b.SuperGLUE_AX_b_ppl_6db806 import AX_b_datasets
+    from ..SuperGLUE_AX_g.SuperGLUE_AX_g_ppl_66caf3 import AX_g_datasets
+    from ..SuperGLUE_BoolQ.SuperGLUE_BoolQ_ppl_314b96 import BoolQ_datasets
+    from ..SuperGLUE_CB.SuperGLUE_CB_ppl_0143fe import CB_datasets
+    from ..SuperGLUE_COPA.SuperGLUE_COPA_ppl_9f3618 import COPA_datasets
+    from ..SuperGLUE_MultiRC.SuperGLUE_MultiRC_ppl_ced824 import MultiRC_datasets
+    from ..SuperGLUE_RTE.SuperGLUE_RTE_ppl_66caf3 import RTE_datasets
+    from ..SuperGLUE_ReCoRD.SuperGLUE_ReCoRD_gen_30dea0 import ReCoRD_datasets
+    from ..SuperGLUE_WiC.SuperGLUE_WiC_ppl_312de9 import WiC_datasets
+    from ..SuperGLUE_WSC.SuperGLUE_WSC_ppl_d0f531 import WSC_datasets
     from ..race.race_ppl_a138cd import race_datasets
     from ..math.math_gen_265cce import math_datasets
     from ..gsm8k.gsm8k_gen_1d7fe4 import gsm8k_datasets
@@ -32,8 +32,10 @@ with read_base():
     from ..piqa.piqa_ppl_1cf9f0 import piqa_datasets
     from ..winogrande.winogrande_ppl_55a66e import winogrande_datasets
     from ..obqa.obqa_ppl_c7c154 import obqa_datasets
-    # from ..nq.nq_gen_c788f6 import nq_datasets
-    # from ..triviaqa.triviaqa_gen_2121ce import triviaqa_datasets
+    from ..nq.nq_gen_c788f6 import nq_datasets
+    from ..triviaqa.triviaqa_gen_2121ce import triviaqa_datasets
     from ..crowspairs.crowspairs_ppl_e811e1 import crowspairs_datasets
+    from ..mmlu.mmlu_ppl import mmlu_datasets
+    from ..agieval.agieval_mixed import agieval_datasets
 
 datasets = sum((v for k, v in locals().items() if k.endswith('_datasets')), [])
--- configs/eval_s.py
+++ configs/eval_s.py
@@ -5,23 +5,23 @@ from opencompass.runners import LocalRunner, SlurmRunner
 from opencompass.tasks import OpenICLInferTask, OpenICLEvalTask
 
 with read_base():
-    from .datasets.collections.C import datasets
-    # from .datasets.collections.base_small import datasets
+    # from .datasets.collections.C import datasets
+    from .datasets.collections.base_small import datasets
     from .models.my_model import models
 
-    from .summarizers.small import summarizer
+    # from .summarizers.small import summarizer
 
 
-work_dir = './outputs/2023_08_01/'
+work_dir = './outputs/2023_08_02/'
 
 infer = dict(
     partitioner=dict(type=SizePartitioner, max_task_size=20000, gen_task_coef=15),
     # partitioner=dict(type='NaivePartitioner'),
     runner=dict(
         type=SlurmRunner,
-        max_num_workers=32,
+        max_num_workers=64,
         task=dict(type=OpenICLInferTask),
-        retry=6),
+        retry=4),
 )
 
 eval = dict(
