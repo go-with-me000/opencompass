36baa7feb24215303bb79f465af315f3782bb8b8
cky <cky3702@163.com>
2023-08-08 15:52:00 +0800

    初步改造完成

---
+++ configs/eval_s.py
@@ -0,0 +1,36 @@
+from mmengine.config import read_base
+
+from opencompass.partitioners import SizePartitioner, NaivePartitioner
+from opencompass.runners import LocalRunner, SlurmRunner
+from opencompass.tasks import OpenICLInferTask, OpenICLEvalTask
+
+with read_base():
+    # from .collections.C_plus import datasets
+    from .collections.example import datasets
+    from .models.modelv1 import models
+
+    # from .summarizers.small import summarizer
+
+
+work_dir = './outputs/2023_08_08/'
+
+infer = dict(
+    partitioner=dict(type=SizePartitioner, max_task_size=20000, gen_task_coef=15),
+    # partitioner=dict(type='NaivePartitioner'),
+    runner=dict(
+        type=SlurmRunner,
+        max_num_workers=16,
+        task=dict(type=OpenICLInferTask),
+        retry=4),
+)
+
+eval = dict(
+    partitioner=dict(type=NaivePartitioner),
+    runner=dict(
+        type=SlurmRunner,
+        max_num_workers=64,
+        task=dict(type=OpenICLEvalTask),
+        retry=4),
+)
+
+# python run.py configs/eval_s.py -p llm -r -l --debug 2>&1 | tee log.txt
