15a397d255828d6af762962e16d08477c553dc14
cky <cky3702@163.com>
2023-08-15 16:09:31 +0800

    add tinystories

--- opencompass/datasets/tinystories.py
+++ opencompass/datasets/tinystories.py
@@ -5,16 +5,21 @@ from opencompass.registry import LOAD_DATASET
 from .base import BaseDataset
 
 
+# Create a tinystoriesDataset class
 @LOAD_DATASET.register_module()
 class tinystoriesDataset(BaseDataset):
 
+    # Load the dataset
     @staticmethod
     def load(**kwargs):
         dataset = load_dataset(**kwargs)
 
+        # Define a preprocess function
         def preprocess(example):
+            # Set the label to 0
             example['label'] = 0
             return example
 
+        # Map the preprocess function to the dataset
         dataset = dataset.map(preprocess)
         return dataset
