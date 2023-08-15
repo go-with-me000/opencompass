8140a140493cf8775433b7d7fc92bef9faace53d
cky <cky3702@163.com>
2023-08-15 16:59:53 +0800

    first

---
+++ configs/collections/evaluation_gen.py
@@ -0,0 +1,10 @@
+from mmengine.config import read_base
+
+with read_base():
+    # from ..datasets.piqa.piqa_gen_evaluation import piqa_datasets
+    # from ..datasets.hellaswag.hellaswag_gen_evaluation import hellaswag_datasets
+    # from ..datasets.commonsenseqa.commonsenseqa_gen_evaluation import commonsenseqa_datasets
+    # from ..datasets.race.race_gen_evaluation import race_datasets
+    from ..datasets.tinystories.tinystories_ppl_evaluation import tinystories_datasets
+
+datasets = sum((v for k, v in locals().items() if k.endswith('_datasets')), [])
--- configs/multimodal/instructblip/README.md
+++
@@ -1,9 +0,0 @@
-# InstructBLIP
-
-### Prepare the environment
-
-```sh
-git clone https://github.com/salesforce/LAVIS.git
-cd ./LAVIS
-pip install -e .
-```
