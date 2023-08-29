67e8c7c9046dc998ff8167a2893a81824e6a629f
陈科宇 <chenkeyu1@pjlab.org.cn>
2023-08-29 03:23:26 +0000

    support InternLM

--- opencompass/models/internal/__init__.py
+++ opencompass/models/internal/__init__.py
@@ -4,3 +4,4 @@ from .openai_allesapin import OpenAIAllesAPIN  # noqa: F401
 from .palm_allesapin import PaLM  # noqa: F401
 from .pjlm import *  # noqa: F401, F403
 from .sensechat import SenseChat  # noqa: F401
+from .intern_model import InternLM  # noqa: F401, F403
