1298df10ee81c078ff9932575566fd7b63727eed
cky <cky3702@163.com>
2023-10-09 12:47:11 +0800

    修改了gitignore

---
+++ configs/example/eval_s.py
@@ -0,0 +1,42 @@
+from mmengine.config import read_base
+
+from opencompass.partitioners import SizePartitioner, NaivePartitioner
+from opencompass.runners import LocalRunner, SlurmRunner
+from opencompass.tasks import OpenICLInferTask, OpenICLEvalTask
+
+with read_base():
+    # from .collections.C_plus import datasets
+    from ..collections.example import datasets
+    # from .collections.base_small import datasets
+    # from .collections.base_small_pre import datasets
+
+    # from .models.llama import models
+    # from .models.model_09_20.models import models
+    from ..models.huggingface import models
+
+    from ..summarizers.small import summarizer
+
+
+
+work_dir = './outputs/2023_10_07/'
+
+infer = dict(
+    partitioner=dict(type=SizePartitioner, max_task_size=4000, gen_task_coef=10),
+    # partitioner=dict(type='NaivePartitioner'),
+    runner=dict(
+        type=SlurmRunner,
+        max_num_workers=100,
+        task=dict(type=OpenICLInferTask),
+        retry=4),
+)
+
+eval = dict(
+    partitioner=dict(type=NaivePartitioner),
+    runner=dict(
+        type=SlurmRunner,
+        max_num_workers=64,
+        task=dict(type=OpenICLEvalTask),
+        retry=3),
+)
+
+# python run.py configs/eval_s.py -p llm  -q spot -r -l --debug 2>&1 | tee log.txt
