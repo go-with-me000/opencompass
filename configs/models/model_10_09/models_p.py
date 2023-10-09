e2c8255304da878f877bf614cff15dabd4533593
cky <cky3702@163.com>
2023-10-09 13:21:05 +0800

    增加了summeraizer

--- configs/models/model_10_09/models_p.py
+++ configs/models/model_10_09/models_p.py
@@ -31,15 +31,32 @@ models = [
     # ),
 ]
 
-for item in range(0, 90):
+# for item in range(0, 90):
+#     model_info = dict(
+#         abbr=f"7B_llama2_coder_sft_v12_random_{100 + item*100}",
+#         type=InternLM,
+#         path=f"/mnt/petrelfs/share/songdemin/model/7B_llama2_coder_sft_v12_random/{100 + item*100}",
+#         tokenizer_path='/mnt/petrelfs/share_data/yanhang/tokenizes/llama.model',
+#         tokenizer_type='llama',
+#         module_path="/mnt/petrelfs/share/songdemin/code/train_internlm/",
+#         model_config="/mnt/petrelfs/share/songdemin/code/train_internlm/configs/7B_llama2_coder_sft_v12_random.py",
+#         max_out_len=100,
+#         max_seq_len=2048,
+#         batch_size=16,
+#         run_cfg=dict(num_gpus=1, num_procs=1)
+#     )
+#     models.append(model_info)
+
+
+for item in range(0, 18):
     model_info = dict(
-        abbr=f"7B_llama2_coder_sft_v12_random_{100 + item*100}",
+        abbr=f"7B_llama2_coder_sft_v15_{500 + item*500}",
         type=InternLM,
-        path=f"/mnt/petrelfs/share/songdemin/model/7B_llama2_coder_sft_v12_random/{100 + item*100}",
+        path=f"/mnt/petrelfs/share/songdemin/model/7B_llama2_coder_sft_v15/{500 + item*500}",
         tokenizer_path='/mnt/petrelfs/share_data/yanhang/tokenizes/llama.model',
         tokenizer_type='llama',
         module_path="/mnt/petrelfs/share/songdemin/code/train_internlm/",
-        model_config="/mnt/petrelfs/share/songdemin/code/train_internlm/configs/7B_llama2_coder_sft_v12_random.py",
+        model_config="/mnt/petrelfs/share/songdemin/code/train_internlm/configs/7B_llama2_coder_sft_v15.py",
         max_out_len=100,
         max_seq_len=2048,
         batch_size=16,
